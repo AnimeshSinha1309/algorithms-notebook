aa10488d473d7171f1ec3e58372d45c663540ee6
Animesh Sinha <animeshsinha1309@gmail.com>
2019-12-25 02:00:10 +0530

    More Compression
    Removed Whitespace in even more files.

--- ref/DisjointSets.cpp
+++ ref/DisjointSets.cpp
@@ -1,36 +1,20 @@
-#include <template.hpp>
+#include "template.hpp"
 
 struct DisjointSetTree {
-    ll comp_count;
-    vector<ll> parent, comp_size;
-    set<ll> roots;
-
+    ll comp_count; vll parent, comp_size; set<ll> roots;
     DisjointSetTree(int n) {
-        comp_count = n;
-        parent.resize(n);
-        comp_size.resize(n, 1);
+        comp_count = n; parent.resize(n); comp_size.resize(n, 1);
         iota(parent.begin(), parent.end(), 0);
-        for (int i = 0; i < n; i++) {
-            roots.insert(i);
-        }
+        for (int i = 0; i < n; i++) roots.insert(i);
     }
-
     int find(int u) {
-        if (parent[u] == u)
-            return parent[u];
+        if (parent[u] == u) return parent[u];
         return parent[u] = find(parent[u]);
     }
-
     bool merge(int u, int v) {
-        u = find(u), v = find(v);
-        if (u == v)
-            return false;
-        parent[u] = v;
-        comp_size[v] += comp_size[u];
-        comp_size[u] = 0;
-        roots.erase(u);
-        comp_count--;
-        return true;
+        u = find(u), v = find(v); if (u == v) return false;
+        parent[u] = v; comp_size[v] += comp_size[u]; comp_size[u] = 0; 
+        roots.erase(u); comp_count--; return true;
     }
 };
 
@@ -38,28 +22,21 @@ class DynamicConnectivity {
     void __dfs(int v, int l, int r, vector<long long>& res) {
         long long last_ans = answer;
         int state = save_ptr;
-        for (auto query : tree[v])
-            merge(query);
-        if (l == r - 1)
-            res[l] = answer;
+        for (auto query : tree[v]) merge(query);
+        if (l == r - 1) res[l] = answer;
         else {
             int m = (l + r) / 2;
             __dfs(v * 2 + 1, l, m, res);
             __dfs(v * 2 + 2, m, r, res);
         }
-        while (save_ptr != state)
-            rollback();
+        while (save_ptr != state) rollback();
         answer = last_ans;
     };
 
    public:
-    int size_nodes;
-    int size_query;
-
-    struct Node {
-        long long parent, comp_size = 1;
-    };
-    long long answer = 0;
+    int size_nodes, size_query;
+    struct Node { long long parent, comp_size = 1; };
+    ll answer = 0;
     vector<Node> data;
     vector<long long*> saved_object;
     vector<long long> saved_value;
@@ -67,96 +44,64 @@ class DynamicConnectivity {
 
     struct Query {
         int u, v;
-        Query(pair<int, int> p = {0, 0}) {
-            u = p.first, v = p.second;
-        }
+        Query(pair<int, int> p = {0, 0}) { u = p.first, v = p.second; }
     };
     vector<vector<Query>> tree;
-
     DynamicConnectivity(int n = 600000, int q = 300000) {
-        size_nodes = n;
-        size_query = q;
-        int tree_size = 1;
-        while (tree_size < q)
-            tree_size <<= 1;
+        size_nodes = n, size_query = q; int tree_size = 1;
+        while (tree_size < q) tree_size <<= 1;
         data = vector<Node>(n);
         tree = vector<vector<Query>>(2 * tree_size);
         saved_object = vector<long long*>(4 * q);
         saved_value = vector<long long>(4 * q);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             data[i].parent = i;
-        }
-        // Storing the initial answer
-        answer = n;
+        answer = n; // Storing the initial answer
     }
-
     void change(long long& x, long long y) {
-        saved_object[save_ptr] = &x;
-        saved_value[save_ptr] = x;
-        x = y;
-        save_ptr++;
+        saved_object[save_ptr] = &x; saved_value[save_ptr] = x;
+        x = y; save_ptr++;
     }
-
     void rollback() {
         save_ptr--;
         (*saved_object[save_ptr]) = saved_value[save_ptr];
     }
-
     int find(int x) {
-        if (data[x].parent == x)
-            return x;
+        if (data[x].parent == x) return x;
         return find(data[x].parent);
     }
-
     void merge(const Query& q) {
-        int x = find(q.u);
-        int y = find(q.v);
-        if (x == y)
-            return;
-        if (data[x].comp_size < data[y].comp_size)
-            swap(x, y);
+        int x = find(q.u); int y = find(q.v);
+        if (x == y) return;
+        if (data[x].comp_size < data[y].comp_size) swap(x, y);
         change(data[y].parent, x);
         change(data[x].comp_size, data[x].comp_size + data[y].comp_size);
         // Changing the Answer on query
         change(answer, answer - 1);
     }
-
     void add(int l, int r, Query edge, int node = 0, int x = 0, int y = -1) {
-        if (y == -1)
-            y = size_query;
-        if (l >= r)
-            return;
-        if (l == x && r == y)
-            tree[node].push_back(edge);
+        if (y == -1) y = size_query;
+        if (l >= r) return;
+        if (l == x && r == y) tree[node].push_back(edge);
         else {
             int m = (x + y) / 2;
             add(l, min(r, m), edge, node * 2 + 1, x, m);
             add(max(m, l), r, edge, node * 2 + 2, m, y);
         }
     }
-
     vector<long long> solve(int v = 0, int l = 0, int r = -1) {
-        if (r == -1)
-            r = size_query;
+        if (r == -1) r = size_query;
         vector<long long> vec(size_query);
-        if (size_query > 0)
-            __dfs(v, l, r, vec);
+        if (size_query > 0) __dfs(v, l, r, vec);
         return vec;
     }
-
-    DynamicConnectivity(int n, vector<Query> queries)
-        : DynamicConnectivity(n, queries.size()) {
+    DynamicConnectivity(int n, vector<Query> queries) : DynamicConnectivity(n, queries.size()) {
         map<pair<int, int>, int> last;
         for (int i = 0; i < size_query; i++) {
             pair<int, int> p(queries[i].u, queries[i].v);
-            if (last.count(p)) {
-                add(last[p], i, queries[i]);
-                last.erase(p);
-            } else {
-                last[p] = i;
-            }
+            if (last.count(p)) { add(last[p], i, queries[i]); last.erase(p); } 
+            else { last[p] = i; }
         }
-        for (auto x : last)
-            add(x.second, size_query, x.first);
+        for (auto x : last) add(x.second, size_query, x.first);
     }
 };
--- ref/StonglyConnected.cpp
+++ ref/StonglyConnected.cpp
@@ -7,41 +7,27 @@ struct DirectedGraph {
     vector<bool> visited;
 
     DirectedGraph(int n) {
-        size = n;
-        order.resize(size);
-        adjacent_f.resize(size);
+        size = n; order.resize(size); adjacent_f.resize(size);
     }
     void add_edge(int v1, int v2) {
-        adjacent_f[v1].push_back(v2);
-        adjacent_r[v2].push_back(v1);
+        adjacent_f[v1].push_back(v2); adjacent_r[v2].push_back(v1);
     }
-
     void _scc_dfs1(int u) {
         visited[u] = 1;
-        for (auto w : adjacent_f[u])
-            if (!visited[w])
-                _scc_dfs1(w);
+        for (auto w : adjacent_f[u]) if (!visited[w]) _scc_dfs1(w);
         order.push_back(u);
     }
     void _scc_dfs2(int u) {
-        visited[u] = 1;
-        comp[u] = curr;
-        comp_nodes[curr].push_back(u);
-        for (auto w : adjacent_r[u])
-            if (!visited[w])
-                _scc_dfs2(w);
+        visited[u] = 1; comp[u] = curr; comp_nodes[curr].push_back(u);
+        for (auto w : adjacent_r[u]) if (!visited[w]) _scc_dfs2(w);
     }
-
     void stongly_connected_components() {
         fill(visited.begin(), visited.end(), false);
         order.clear();
-        for (int i = 0; i < size; i++)
-            if (!visited[i])
-                _scc_dfs1(i);
+        for (int i = 0; i < size; i++) if (!visited[i]) _scc_dfs1(i);
         fill(visited.begin(), visited.end(), false);
         reverse(order.begin(), order.end());
         curr = 0;
-        // components are generated in topological order (Kosaraju)
         for (auto u : order)
             if (!visited[u])
                 comp_nodes[++curr].clear(), _scc_dfs2(u);
@@ -66,39 +52,17 @@ struct Satisfiability : DirectedGraph {
         for (int i = 1; i <= curr; i++)
             for (auto it : comp_nodes[i]) {
                 int u = it >> 1;
-                if (val[u])
-                    continue;
+                if (val[u]) continue;
                 val[u] = (it & 1 ? +1 : -1);
             }
         return val;
     }
-    int var(int x) {
-        return x << 1;
-    }
-    int NOT(int x) {
-        return x ^ 1;
-    }
-    void add_imp(int v1, int v2) {
-        add_edge(v1, v2);
-        add_edge(1 ^ v2, 1 ^ v1);
-    }
-    void add_equiv(int v1, int v2) {
-        add_imp(v1, v2);
-        add_imp(v2, v1);
-    }
-    void add_or(int v1, int v2) {
-        add_edge(1 ^ v1, v2);
-        add_edge(1 ^ v2, v1);
-    }
-    void add_xor(int v1, int v2) {
-        add_or(v1, v2);
-        add_or(1 ^ v1, 1 ^ v2);
-    }
-    void add_true(int v1) {
-        add_edge(1 ^ v1, v1);
-    }
-    void add_and(int v1, int v2) {
-        add_true(v1);
-        add_true(v2);
-    }
+    int var(int x) { return x << 1; }
+    int NOT(int x) { return x ^ 1; }
+    void add_imp(int v1, int v2) { add_edge(v1, v2); add_edge(1 ^ v2, 1 ^ v1); }
+    void add_equiv(int v1, int v2) { add_imp(v1, v2); add_imp(v2, v1); }
+    void add_or(int v1, int v2) { add_edge(1 ^ v1, v2); add_edge(1 ^ v2, v1); }
+    void add_xor(int v1, int v2) { add_or(v1, v2); add_or(1 ^ v1, 1 ^ v2); }
+    void add_true(int v1) { add_edge(1 ^ v1, v1); }
+    void add_and(int v1, int v2) { add_true(v1); add_true(v2); }
 };
