8c1bc2494035388efc0b3bba9192d3434b3ce062
GaurangTandon <gaurang.tandon@students.iiit.ac.in>
2019-12-25 19:00:25 +0530

    add push relabel and poly shifting trick

--- ref/FlowAlgorithms.cpp
+++ ref/FlowAlgorithms.cpp
@@ -1,5 +1,95 @@
 #include "template.hpp"
 
+#define AVG_CASE
+#ifdef AVG_CASE
+struct Edge {
+    ll from, to, cap, flow, index;
+    Edge(ll dfrom, ll dto, ll dcap, ll dflow, ll dindex)
+        : from(dfrom), to(dto), cap(dcap), flow(dflow), index(dindex) {
+    }
+};
+struct PushRelabel {
+    ll N;
+    vector<vector<Edge>> G;
+    vector<ll> excess;
+    vector<ll> dist, active, count;
+    queue<ll> Q;
+    PushRelabel(ll dN)
+        : N(dN), G(N), excess(N), dist(N), active(N), count(2 * N) {
+    }
+    void addEdge(ll from, ll to, ll cap) {
+        G[from].push_back(Edge(from, to, cap, 0, G[to].size()));
+        if (from == to)
+            G[from].back().index++;
+        G[to].push_back(Edge(to, from, 0, 0, G[from].size() - 1));
+    }
+    void Enqueue(ll v) {
+        if (!active[v] && excess[v] > 0) {
+            active[v] = true;
+            Q.push(v);
+        }
+    }
+    void Push(Edge &e) {
+        ll amt = ll(min(excess[e.from], ll(e.cap - e.flow)));
+        if (dist[e.from] <= dist[e.to] || amt == 0)
+            return;
+        e.flow += amt;
+        G[e.to][e.index].flow -= amt;
+        excess[e.to] += amt;
+        excess[e.from] -= amt;
+        Enqueue(e.to);
+    }
+    void Gap(ll k) {
+        for (ll v = 0; v < N; v++) {
+            if (dist[v] < k)
+                continue;
+            count[dist[v]]--;
+            dist[v] = max(dist[v], N + 1);
+            count[dist[v]]++;
+            Enqueue(v);
+        }
+    }
+    void Relabel(ll v) {
+        count[dist[v]]--;
+        dist[v] = 2 * N;
+        for (ll i = 0; i < (ll)G[v].size(); i++)
+            if (G[v][i].cap - G[v][i].flow > 0)
+                dist[v] = min(dist[v], dist[G[v][i].to] + 1);
+        count[dist[v]]++;
+        Enqueue(v);
+    }
+    void Discharge(ll v) {
+        for (ll i = 0; excess[v] > 0 && i < (ll)G[v].size(); i++)
+            Push(G[v][i]);
+        if (excess[v] > 0) {
+            if (count[dist[v]] == 1)
+                Gap(dist[v]);
+            else
+                Relabel(v);
+        }
+    }
+    ll maxFlow(ll s, ll t) {
+        count[0] = N - 1;
+        count[N] = 1;
+        dist[s] = N;
+        active[s] = active[t] = true;
+        for (ll i = 0; i < (ll)G[s].size(); i++) {
+            excess[s] += G[s][i].cap;
+            Push(G[s][i]);
+        }
+        while (!Q.empty()) {
+            ll v = Q.front();
+            Q.pop();
+            active[v] = false;
+            Discharge(v);
+        }
+        ll totflow = 0;
+        for (ll i = 0; i < (ll)G[s].size(); i++)
+            totflow += G[s][i].flow;
+        return totflow;
+    }
+};
+#else
 struct Edge {
     int u, v;
     ll cap, flow;
@@ -72,7 +162,7 @@ struct Dinic {
         return total;
     }
 };
-
+#endif
 class HopcroftKarp {
    public:
     static const int INF = 1e9;
